782add19cf3df7e27131056e847bc447a85a7bf9
baconfig <133508186+baconfig@users.noreply.github.com>
2023-05-14 14:26:38 +0700

    Update ins-xray.sh

--- ins-xray.sh
+++ ins-xray.sh
@@ -129,3 +129,55 @@ apt install -y nginx
 cd
 rm -fr /etc/nginx/sites-enabled/default
 rm -fr /etc/nginx/sites-available/default
+wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/baconfig/config/main/nginx.conf.txt"
+mkdir -p /home/vps/public_html
+wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/baconfig/config/main/vps.conf.txt"
+wget -q -O /etc/nginx/conf.d/xray.conf "https://raw.githubusercontent.com/baconfig/config/main/xray.conf.txt"
+wget -q -O xraymode.sh https://raw.githubusercontent.com/baconfig/config/main/xraymode.sh && chmod +x xraymode.sh && ./xraymode.sh
+sleep 1
+wget -q -O xray.conf https://raw.githubusercontent.com/baconfig/config/main/xray.conf.txt && chmod +x xray.conf && ./xray.conf
+sleep 1
+
+# Installing Xray Service
+rm -fr /etc/systemd/system/xray.service.d
+#rm -fr /etc/systemd/system/xray.service
+cat <<EOF> /etc/systemd/system/xray.service
+Description=Xray Service
+Documentation=https://github.com/xtls
+After=network.target nss-lookup.target
+
+[Service]
+User=www-data
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE                            
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/local/bin/xray run -config /etc/xray/config.json
+Restart=on-failure
+RestartPreventExitStatus=23
+LimitNPROC=10000
+LimitNOFILE=1000000
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo -e "[ ${GREEN}ok${NC} ] Enable & Start & Restart & Xray"
+systemctl daemon-reload >/dev/null 2>&1
+systemctl enable xray >/dev/null 2>&1
+systemctl start xray >/dev/null 2>&1
+systemctl restart xray >/dev/null 2>&1
+echo -e "[ ${GREEN}ok${NC} ] Enable & Start & Restart & Nginx"
+systemctl daemon-reload >/dev/null 2>&1
+systemctl enable nginx >/dev/null 2>&1
+systemctl start nginx >/dev/null 2>&1
+systemctl restart nginx >/dev/null 2>&1
+# Restart All Service
+echo -e "$yell[SERVICE]$NC Restart All Service"
+sleep 1
+chown -R www-data:www-data /home/vps/public_html
+# Enable & Restart & Xray & Trojan & Nginx
+sleep 1
+echo -e "[ ${GREEN}ok${NC} ] Restart & Xray & Nginx"
+systemctl daemon-reload >/dev/null 2>&1
+systemctl restart xray >/dev/null 2>&1
+systemctl restart nginx >/dev/null 2>&1
